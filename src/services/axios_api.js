bab2fe69e7f16f23448ce159fc3ee96217c70ff6
Rômulo Fernando <fssromulo@gmail.com>
2019-11-20 23:43:29 -0300

    Cors Amazon Api itunes 5

--- src/services/axios_api.js
+++ src/services/axios_api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
 	baseURL: 'https://itunes.apple.com/',
 	headers: {
-		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Origin': 'https://master.d2p2cncjbq20vv.amplifyapp.com/',
 		'Content-Type': 'application/json;charset=utf-8',
 		'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
 		'Access-Control-Allow-Credentials': true,
