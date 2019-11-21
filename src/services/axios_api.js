ae85c1f0ddc295fcf7e88bb593b0b2d7490816ae
Rômulo Fernando <fssromulo@gmail.com>
2019-11-20 22:54:55 -0300

    Cors Amazon Api itunes 3

--- src/services/axios_api.js
+++ src/services/axios_api.js
@@ -4,7 +4,8 @@ const api = axios.create({
 	baseURL: 'https://itunes.apple.com/',
 	headers: {
 		'Access-Control-Allow-Origin': '*',
-		'Content-Type': 'application/json;charset=utf-8'
+		'Content-Type': 'application/json;charset=utf-8',
+		'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
 	}
 });
 
